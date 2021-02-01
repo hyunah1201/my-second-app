71d2344dc7ead0ceb057981264b5188703e00564
hyunah1201 <youngnrichhh@naver.com>
2021-02-01 15:59:31 +0900

    added a new end-point

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req,res) => res.send('Hello, Nodejs!'));
+app.get('/welcome', (req,res) => res.send('Hi,there. Welcome to the Nodejs service.'));
+app.get('/check', (req,res) => res.send('Example app listening on port 3000'));
 app.listen(port, () => console.log('Example app listening on port 3000'))
